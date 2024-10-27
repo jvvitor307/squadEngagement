74cad60a67b9d7864f36fa45d33fc552807eec60
joao vitor melo <jvvitor307@gmail.com>
2024-10-27 15:46:38 -0300

    feat: add controller.py do squad e exceções no services.py do squad

--- handle/Squad/services.py
+++ handle/Squad/services.py
@@ -6,31 +6,41 @@ from handle.Squad.models import SquadCreate
 class SquadServices:
 
     def add(self, db: Session, obj: SquadCreate):
-        novo = Squad(
-            squad_name = obj.squad_name
-        )
-        db.add(novo)
-        db.commit()
-        db.refresh(novo)
-        return novo
+        try:
+            novo = Squad(
+                squad_name = obj.squad_name
+            )
+            db.add(novo)
+            db.commit()
+            db.refresh(novo)
+            return novo
+        except:
+            db.rollback()
 
     def get_by_id(self, db: Session, id: int):
         return db.query(Squad).filter(Squad.id == id).first()
 
-    def get_all(self, db: Session, id: int):
+    def get_all(self, db: Session):
         return db.query(Squad).all()
 
     def update(self, db: Session, squad: Squad, SquadModel: SquadCreate):
-        squad.squad_name = SquadModel.squad_name
-        db.commit()
-        db.refresh(squad)
+        try:
+            squad.squad_name = SquadModel.squad_name
+            db.commit()
+            db.refresh(squad)
+        except:
+            db.rollback()
+
 
     def delete(self, db: Session, squad: Squad):
-        db.delete(squad)
-        db.commit()
-        db.refresh(squad)
+        try:
+            db.delete(squad)
+            db.commit()
+            db.refresh(squad)
+        except:
+            db.rollback()
 
-    def addPessoas(self, db: Session, squad: Squad, person: Person):
+
 
 
 
