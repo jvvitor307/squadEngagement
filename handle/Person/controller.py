b4a89985794b5a059ad8823e2bb36126f201ea72
joao vitor melo <jvvitor307@gmail.com>
2024-10-27 15:52:35 -0300

    feat: add controller.py do person e exceções no services.py do person

---
+++ handle/Person/controller.py
@@ -0,0 +1,53 @@
+from fastapi import APIRouter, HTTPException, Depends, status
+from sqlalchemy.orm import Session
+from typing import List
+from handle.Person.models import PersonCreate, PersonView
+from handle.Person.services import PersonServices as service
+from database.utils import get_db
+
+router = APIRouter(prefix="/app/squad", tags=["Servidores"])
+
+exception_nao_encontado = HTTPException(
+    status_code=status.HTTP_404_NOT_FOUND, detail="Não encontrado"
+)
+
+
+@router.get("/", response_model=List[PersonView])
+def get_all(db: Session = Depends(get_db)):
+    return service.get_all(db=db)
+
+
+@router.get("/{id}", response_model=PersonView)
+def get_by_id(id: int, db: Session = Depends(get_db)):
+    obj = service.get_by_id(db, id)
+    if not obj:
+        raise exception_nao_encontado
+
+    return obj
+
+
+
+@router.post("/", response_model=PersonView, status_code=status.HTTP_201_CREATED)
+def create(person: PersonCreate, db: Session = Depends(get_db)):
+    novo = service.add(db, person)
+    return novo
+
+
+@router.put("/{id}", response_model=PersonView)
+def update(id: int, obj: PersonCreate, db: Session = Depends(get_db)):
+    obj_to_update = service.get_by_id(db, id)
+    if not obj_to_update:
+        raise exception_nao_encontado
+
+    service.update(db, obj_to_update, obj)
+    return obj_to_update
+
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete(id: int, db: Session = Depends(get_db)):
+    obj_to_delete = service.get_by_id(db, id)
+    if not obj_to_delete:
+        raise exception_nao_encontado
+
+    service.delete(db, obj_to_delete)
+    return obj_to_delete
--- handle/Person/services.py
+++ handle/Person/services.py
@@ -6,14 +6,17 @@ from handle.Person.models import PersonCreate
 class PersonServices:
 
     def add(self, db: Session, obj: PersonCreate):
-        novo = Person(
-            first_name=obj.first_name,
-            last_name=obj.last_name,
-        )
-        db.add(novo)
-        db.commit()
-        db.refresh(novo)
-        return novo
+        try:
+            novo = Person(
+                first_name=obj.first_name,
+                last_name=obj.last_name,
+            )
+            db.add(novo)
+            db.commit()
+            db.refresh(novo)
+            return novo
+        except:
+            db.rollback()
 
     def get_by_id(self, db: Session, id: int):
         return db.query(Person).filter(Person.id == id).first()
@@ -22,15 +25,21 @@ class PersonServices:
         return db.query(Person).all()
 
     def update(self, db: Session, person: Person, PersonModel: PersonCreate):
-        person.first_name = PersonModel.first_name
-        person.last_name = PersonModel.last_name
-        db.commit()
-        db.refresh(person)
+        try:
+            person.first_name = PersonModel.first_name
+            person.last_name = PersonModel.last_name
+            db.commit()
+            db.refresh(person)
+        except:
+            db.rollback()
 
     def delete(self, db: Session, person: Person):
-        db.delete(person)
-        db.commit()
-        db.refresh(person)
+        try:
+            db.delete(person)
+            db.commit()
+            db.refresh(person)
+        except:
+            db.rollback()
 
 
 
